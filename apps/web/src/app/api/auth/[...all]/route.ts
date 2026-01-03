c62715d406bc93b5f61402624a0225b4722cbc37
AleYepes <73545779+AleYepes@users.noreply.github.com>
2026-01-03 17:51:27 +0100

    init

---
+++ apps/web/src/app/api/auth/[...all]/route.ts
@@ -0,0 +1,4 @@
+import { auth } from "@micarnet/auth";
+import { toNextJsHandler } from "better-auth/next-js";
+
+export const { GET, POST } = toNextJsHandler(auth.handler);
---
+++ packages/auth/src/index.ts
@@ -0,0 +1,19 @@
+import { nextCookies } from 'better-auth/next-js';
+import { db } from "@micarnet/db";
+import * as schema from "@micarnet/db/schema/auth";
+import { env } from "@micarnet/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [nextCookies()]
+});
