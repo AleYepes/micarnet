1364d7ebdf2e7a7d0b32b6b82785e68d7c171b55
AleYepes <73545779+AleYepes@users.noreply.github.com>
2026-01-03 18:27:37 +0100

    build: fix husky pre-commit hook

--- apps/web/src/app/dashboard/dashboard.tsx
+++ apps/web/src/app/dashboard/dashboard.tsx
@@ -1,15 +1,20 @@
 "use client";
 import { useQuery } from "@tanstack/react-query";
 
-import { authClient } from "@/lib/auth-client";
+import type { authClient } from "@/lib/auth-client";
 import { orpc } from "@/utils/orpc";
 
-export default function Dashboard({ session }: { session: typeof authClient.$Infer.Session }) {
+export default function Dashboard({
+  session,
+}: {
+  session: typeof authClient.$Infer.Session;
+}) {
   const privateData = useQuery(orpc.privateData.queryOptions());
 
   return (
-    <>
+    <div>
+      <p>Logged in as: {session.user.email}</p>
       <p>API: {privateData.data?.message}</p>
-    </>
+    </div>
   );
 }
--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,19 +1,19 @@
-import { nextCookies } from 'better-auth/next-js';
 import { db } from "@micarnet/db";
 import * as schema from "@micarnet/db/schema/auth";
 import { env } from "@micarnet/env/server";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
 
-    schema: schema,
+    schema,
   }),
   trustedOrigins: [env.CORS_ORIGIN],
   emailAndPassword: {
     enabled: true,
   },
-  plugins: [nextCookies()]
+  plugins: [nextCookies()],
 });
